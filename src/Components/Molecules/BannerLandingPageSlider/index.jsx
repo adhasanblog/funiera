ea25bf6dbbcbeb2a7a8f9e43b5001f678f45f07a
adhasanblog <fhasanblog@gmail.com>
2022-08-16 17:22:52 +0700

    funyera app

--- src/Components/Molecules/BannerLandingPageSlider/index.jsx
+++ src/Components/Molecules/BannerLandingPageSlider/index.jsx
@@ -1,13 +1,49 @@
+import SwiperCore, {
+  Autoplay,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow } from 'swiper';
+import 'swiper/css';
 
 const BannerLandingPageSlider = () => {
   return (
-    <Swiper>
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
+      effect='coverflow'
+      coverflowEffect={{
+        rotate: 0,
+        depth: 0,
+        slideShadows: false,
+        scale: 1.5,
+      }}
+      width={480}
+      height={600}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
       <SwiperSlide>
-        {({ isActive }) => (
-          <div>Current slide is {isActive ? 'active' : 'not active'}</div>
-        )}
+        <img src='https://swiperjs.com/demos/images/nature-1.jpg' alt='' />
       </SwiperSlide>
+      <SwiperSlide>
+        <img src='https://swiperjs.com/demos/images/nature-2.jpg' alt='' />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src='https://swiperjs.com/demos/images/nature-3.jpg' alt='' />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src='https://swiperjs.com/demos/images/nature-4.jpg' alt='' />
+      </SwiperSlide>
+      ...
     </Swiper>
   );
 };
